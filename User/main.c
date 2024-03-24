dcaab4eeac2206ef7110860eba899e1db055e06a
Mahmood_Reda <mr4141705@gmail.com>
2024-03-24 13:16:25 +0300

    Editing uart_config  (baud_rate from 115200  to 921600 )

--- User/main.c
+++ User/main.c
@@ -23,17 +23,20 @@ int main(void){
 }
 
 /*-----------------------------------------------------------*/
-extern int r ,i ;
+//char Data[20];
 /* User Task */
 void UserTask(void *argument){
 
 ESP_Boot();
 ESP_Reset();
 WIFI_AP_SOCKET("Redaa", "00000000");
+//BLE_ServerMode("redaas");
 //	 put your code here, to run repeatedly.
 	while(1){
 //		HAL_Delay(2000);
-		SOCKET_WRITE("123456789qwertyuiopasdfghjklzxcvbnm");
+//		BLE_Write("fgh", server);
+//		BLE_Read(Data, server);
+		SOCKET_WRITE("Hexa");
 		SOCKET_READ(Data);
 	}
 }
