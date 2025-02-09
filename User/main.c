f4c6633ffca3717b45dedc5d060770e9f95cc39e
Mahmood_Reda <mr4141705@gmail.com>
2025-02-09 12:33:22 +0300

    Increased SystemClock from 48MHz to 64MHz. Changed PLL settings: replaced PREDIV/PLLMUL with PLLM/PLLN/PLLP .

--- User/main.c
+++ User/main.c
@@ -13,7 +13,7 @@
 /* Private function prototypes -----------------------------------------------*/
 
 /* Main function ------------------------------------------------------------*/
-char Data[100];
+
 int main(void){
 
 	Module_Init();		//Initialize Module &  BitzOS
@@ -21,7 +21,7 @@ int main(void){
 	//Don't place your code here.
 	for(;;){}
 }
-
+uint32_t apb1ClockFreq, ahbClockFreq, systemClockFreq;
 /*-----------------------------------------------------------*/
 /* User Task */
 void UserTask(void *argument){
@@ -30,6 +30,17 @@ ESP_Reset();
 
 //	 put your code here, to run repeatedly.
 	while(1){
+
+		// Get the APB1 peripheral clock frequency (used for peripherals like UART and TIMx in some cases)
+		 apb1ClockFreq = HAL_RCC_GetPCLK1Freq();
+
+		// Get the AHB clock frequency (HCLK), which is used for the CPU, memory, and other controllers
+		 ahbClockFreq = HAL_RCC_GetHCLKFreq();
+
+		// Get the system clock frequency (SYSCLK), which serves as the main clock source for the system
+		 systemClockFreq = HAL_RCC_GetSysClockFreq();
+
+
 
 	}
 }
