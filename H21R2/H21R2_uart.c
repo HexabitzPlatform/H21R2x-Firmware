dc982722fd8bccd06d12f6b23a6cdd4fee5a5a5c
Mahmood_Reda <mr4141705@gmail.com>
2025-02-18 14:07:18 +0300

    Fixed DMA issues and enabled APIS Bridge Unbridge  StartScastDMAStream .

--- H21R2/H21R2_uart.c
+++ H21R2/H21R2_uart.c
@@ -200,6 +200,8 @@ void MX_USART3_UART_Init(void)
   /* USER CODE END USART3_Init 2 */
 
 }
+
+int  q ;
 void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 	
 	  GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -240,6 +242,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		hdma_usart1_rx.Init.Mode = DMA_CIRCULAR;
 		hdma_usart1_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+		msgRxDMA[(GetPort(huart)-1)] = &hdma_usart1_rx;
+
 		HAL_DMA_Init(&hdma_usart1_rx);
 
 		__HAL_LINKDMA(huart, hdmarx, hdma_usart1_rx);
@@ -286,6 +291,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 			hdma_usart2_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 			hdma_usart2_rx.Init.Mode = DMA_CIRCULAR;
 			hdma_usart2_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+			msgRxDMA[(GetPort(huart)-1)] = &hdma_usart2_rx;
+
 			HAL_DMA_Init(&hdma_usart2_rx);
 
 			__HAL_LINKDMA(huart, hdmarx, hdma_usart2_rx);
@@ -357,6 +365,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		    hdma_usart3_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		    hdma_usart3_rx.Init.Mode = DMA_CIRCULAR;
 		    hdma_usart3_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+
+
 		    HAL_DMA_Init(&hdma_usart3_rx) ;
 
 		    __HAL_LINKDMA(huart,hdmarx,hdma_usart3_rx);
@@ -401,6 +412,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		    hdma_usart4_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		    hdma_usart4_rx.Init.Mode = DMA_CIRCULAR;
 		    hdma_usart4_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+			msgRxDMA[(GetPort(huart)-1)] = &hdma_usart4_rx;
+
 		    HAL_DMA_Init(&hdma_usart4_rx);
 		
 		    __HAL_LINKDMA(huart,hdmarx,hdma_usart4_rx);
@@ -445,6 +459,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		hdma_usart5_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		hdma_usart5_rx.Init.Mode = DMA_CIRCULAR;
 		hdma_usart5_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+		msgRxDMA[(GetPort(huart)-1)] = &hdma_usart5_rx;
+
 		HAL_DMA_Init(&hdma_usart5_rx);
 
 		__HAL_LINKDMA(huart, hdmarx, hdma_usart5_rx);
@@ -483,6 +500,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		    hdma_usart6_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		    hdma_usart6_rx.Init.Mode = DMA_CIRCULAR;
 		    hdma_usart6_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+			msgRxDMA[(GetPort(huart)-1)] = &hdma_usart6_rx;
+
 	   	    HAL_DMA_Init(&hdma_usart6_rx);
 
 		    __HAL_LINKDMA(huart,hdmarx,hdma_usart6_rx);
