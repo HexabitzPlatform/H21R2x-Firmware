9206b08e671ff2bd21ea2aeea9107d79afc1371e
Ahmad Abdulhai <@Aa52585258aA@>
2025-07-07 10:33:17 +0300

    Fixing bug in "USART6_AF" macro

--- H21R2/H21R2.h
+++ H21R2/H21R2.h
@@ -81,7 +81,7 @@
 #define	USART6_RX_PIN		GPIO_PIN_5
 #define	USART6_TX_PORT		GPIOA
 #define	USART6_RX_PORT		GPIOA
-#define	USART6_AF			GPIO_AF8_USART6
+#define	USART6_AF			GPIO_AF3_USART6
 
 /* ESP32 UART Pin Definitions */
 #define ESP32_UART_RX_PIN      GPIO_PIN_9
